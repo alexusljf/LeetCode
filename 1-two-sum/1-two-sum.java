29cba4fb8802489c14a5392d260eca16c33e577d
Alexus <97541746+alexusljf@users.noreply.github.com>
2022-10-20 14:05:17 +0800

    Time: 2226 ms (5.03%), Space: 48 MB (5.28%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,16 +1,22 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int i,j;
         int[] result = new int[2];
+        // Use HashMap for O(1) constant time access
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>(); // Syntax is Key, Value
+    	int i;
+    	for(i=0;i<nums.length;i++) {
+    		// Key is item, value is index
+    		map.put(nums[i], i);
+    	}
         for(i=0;i<nums.length;i++) {
-        	for(j=i+1;j<nums.length;j++) {
-        			if(nums[i] + nums[j] == target) {
-        				result[0] = i;
-        				result[1] = j;
-                        break;
-        			}
+        	int need = target - nums[i];
+        	for(Integer key: map.keySet()) {
+        		if(need==key && map.get(key) != i) {
+        			result[0] = i;
+        			result[1] = map.get(key); // map.get(key) to get the index value
         		}
         	}
+        }
         return result;
-}
+    }
 }
