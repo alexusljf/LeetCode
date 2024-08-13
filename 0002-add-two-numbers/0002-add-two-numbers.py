1b94e5c60612bde2fd8960271da8304daf9deda5
Alexus Lim <97541746+alexusljf@users.noreply.github.com>
2024-08-13 17:58:40 +0800

    Time: 52 ms (21.39%), Space: 11.7 MB (31.04%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -10,47 +10,17 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
-        if l1 and l2 == None:
-            return None
-        if l1 == None:
-            return l2
-        if l2 == None:
-            return l1
-        curl1 = l1
-        curl2 = l2
-        returnHead = ListNode(0,None)
-        returnCur = returnHead
         carry=0
-        while curl1 != None or curl2 != None:
-            if curl1 == None:
-                sum = curl2.val + carry
-                if sum<10:
-                    carry=0
-                else:
-                    sum-=10
-                    carry=1
-            elif curl2 == None:
-                sum = curl1.val + carry
-                if sum<10:
-                    carry=0
-                else:
-                    sum-=10
-                    carry=1
-            else:
-                sum = curl1.val + curl2.val + carry
-                if sum < 10:
-                    carry = 0
-                else:
-                    sum -= 10
-                    carry = 1
-            returnCur.next = ListNode(sum)
-            if curl1 != None:
-                curl1 = curl1.next
-            if curl2 != None:
-                curl2=curl2.next
+        returnHead=ListNode(0)
+        returnCur=returnHead
+        while l1 or l2 or carry:
+            sum = (l1.val if l1 else 0) + (l2.val if l2 else 0) + carry
+            carry = sum // 10
+            digit = sum % 10
+            returnCur.next = ListNode(digit)
             returnCur = returnCur.next
-        if curl1 == None and curl2 == None and carry:
-            returnCur.next=ListNode(carry)
+            if l1:
+                l1=l1.next
+            if l2:
+                l2=l2.next
         return returnHead.next
-        
-        
