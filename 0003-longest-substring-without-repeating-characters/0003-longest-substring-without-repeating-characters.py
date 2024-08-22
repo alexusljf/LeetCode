75aa1456c0c427ce6217802a10a3bf613d817a23
Alexus Lim <97541746+alexusljf@users.noreply.github.com>
2024-08-22 16:24:49 +0800

    Time: 52 ms (72.52%), Space: 16.6 MB (38.22%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -4,6 +4,9 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+        
+        '''
+        sliding window using set
         # use a set to store unique chars, iterate thru s
         char_set = set()
         l,length = 0,0
@@ -15,4 +18,17 @@ class Solution(object):
             char_set.add(s[r])
             length = max(length,r-l+1)
         return length
+        '''
         
+        # sliding window using hashmap
+        # use hashmap to store char, index pairs
+        char_index_map = {}
+        l,length=0,0
+        for r in range(len(s)):
+            # if the cur char is in the map and the index is within the sliding window (>=l), 
+            # then we increment l to start after the prev occurence of the char
+            if s[r] in char_index_map and char_index_map[s[r]] >= l:
+                l = char_index_map[s[r]] + 1
+            char_index_map[s[r]] = r
+            length = max(length, r-l+1)
+        return length
