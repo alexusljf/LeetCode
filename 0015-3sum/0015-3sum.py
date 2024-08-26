efb1bb60d729ed25f649527ae01cceee8a9cdacb
Alexus Lim <97541746+alexusljf@users.noreply.github.com>
2024-08-27 01:31:45 +0800

    Time: 765 ms (34.41%), Space: 19.9 MB (94.66%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -3,6 +3,7 @@ class Solution:
         returnList = list()
         nums_sorted = sorted(nums)
         for i in range(len(nums_sorted)-2):
+            # check if cur is same as prev, we skip it as the triplets for the i has already been calculated
             if i >0 and  nums_sorted[i] == nums_sorted[i-1]:
                 continue
             l,r=i+1,len(nums_sorted)-1
@@ -10,10 +11,13 @@ class Solution:
                 total = nums_sorted[i]+nums_sorted[l]+nums_sorted[r]
                 if total == 0:
                     returnList.append((nums_sorted[i],nums_sorted[l],nums_sorted[r]))
+                    # skipping dupes for left and right pointer
+                    while l<r and nums_sorted[l] == nums_sorted[l+1]:
+                        l += 1
+                    while l<r and nums_sorted[r] == nums_sorted[r-1]:
+                        r -= 1    
                     l+=1
                     r-=1
-                    while nums_sorted[l] == nums_sorted[l-1] and l<r:
-                        l+=1
                 if total < 0:
                     l+=1
                 elif total > 0:
