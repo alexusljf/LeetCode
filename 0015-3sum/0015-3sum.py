4968ba94e8ab255b01f17f95c483ad8c2c1f4f7f
Alexus Lim <97541746+alexusljf@users.noreply.github.com>
2024-08-27 01:27:46 +0800

    Time: 789 ms (32.93%), Space: 19.9 MB (94.66%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,18 +1,21 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        if nums == [0] * len(nums):
-            return [[0,0,0]]
-        
-        returnSet = set()
+        returnList = list()
         nums_sorted = sorted(nums)
         for i in range(len(nums_sorted)-2):
+            if i >0 and  nums_sorted[i] == nums_sorted[i-1]:
+                continue
             l,r=i+1,len(nums_sorted)-1
             while l<r:
-                if nums_sorted[i]+nums_sorted[l]+nums_sorted[r] == 0:
-                    returnSet.add((nums_sorted[i],nums_sorted[l],nums_sorted[r]))
+                total = nums_sorted[i]+nums_sorted[l]+nums_sorted[r]
+                if total == 0:
+                    returnList.append((nums_sorted[i],nums_sorted[l],nums_sorted[r]))
                     l+=1
-                if nums_sorted[i]+nums_sorted[l]+nums_sorted[r] < 0:
-                    l+=1
-                elif nums_sorted[i]+nums_sorted[l]+nums_sorted[r] > 0:
                     r-=1
-        return list(returnSet)
+                    while nums_sorted[l] == nums_sorted[l-1] and l<r:
+                        l+=1
+                if total < 0:
+                    l+=1
+                elif total > 0:
+                    r-=1
+        return returnList
