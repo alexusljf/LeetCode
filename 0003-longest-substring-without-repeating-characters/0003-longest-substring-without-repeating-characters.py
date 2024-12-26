18de80a6ae8216180b00d5a818177fc5827ad127
Alexus Lim <97541746+alexusljf@users.noreply.github.com>
2024-12-26 15:41:18 +0800

    0003-LONGEST-SUBSTRING-WITHOUT-REPEATING-CHARACTERS Stats: Time: 15 ms (81.07%), Space: 18 MB (6.92%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -5,30 +5,15 @@ class Solution(object):
         :rtype: int
         """
         
-        '''
-        sliding window using set
-        # use a set to store unique chars, iterate thru s
-        char_set = set()
-        l,length = 0,0
+        substringStart, result = 0, 0
+        # hash map to store char, index pair
+        charMap = {}
         for r in range(len(s)):
-            # if cur char is in the set, we remove all chars from left until the set contains no duplicates
-            while s[r] in char_set:
-                char_set.remove(s[l])
-                l+=1
-            char_set.add(s[r])
-            length = max(length,r-l+1)
-        return length
-        '''
-        
-        # sliding window using hashmap
-        # use hashmap to store char, index pairs
-        char_index_map = {}
-        l,length=0,0
-        for r in range(len(s)):
-            # if the cur char is in the map and the index is within the sliding window (>=l), 
-            # then we increment l to start after the prev occurence of the char
-            if s[r] in char_index_map and char_index_map[s[r]] >= l:
-                l = char_index_map[s[r]] + 1
-            char_index_map[s[r]] = r
-            length = max(length, r-l+1)
-        return length
+            # if char is present in map and after the start of current substring, we set start position to after the previous occurence
+            if s[r] in charMap and charMap[s[r]] >= substringStart:
+                substringStart = charMap[s[r]] + 1
+            # add the char:index pair to map
+            charMap[s[r]] = r
+            result = max(result, r - substringStart + 1)
+
+        return result
