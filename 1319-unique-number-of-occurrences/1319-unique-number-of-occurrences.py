e5eaa80805197558136e235ba805637f7521148f
Alexus Lim <97541746+alexusljf@users.noreply.github.com>
2025-03-21 15:39:25 +0800

    1319-UNIQUE-NUMBER-OF-OCCURRENCES Stats: Time: 0 ms (100%), Space: 18.2 MB (20.86%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.py
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.py
@@ -1,14 +1,5 @@
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        hashset = {}
-        # store the occurence
-        for i in arr:
-            if i not in hashset:
-                hashset[i] = 1
-            else:
-                hashset[i]+=1
-        occurences = sorted(hashset.values())
-        for i in range(1,len(occurences)):
-            if occurences[i] == occurences[i-1]:
-                return False
-        return True
+        count = Counter(arr)
+        print(count, set(count.values()), len(count))
+        return (len(set(count.values())) == len(count))
