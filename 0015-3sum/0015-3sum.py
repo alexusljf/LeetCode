f7b8651466e27ebc3c8512ff91a5435fa6d8e9e3
Alexus Lim <97541746+alexusljf@users.noreply.github.com>
2025-03-28 22:00:44 +0800

    0015-3SUM Stats: Time: 623 ms (37.35%), Space: 20.6 MB (69.12%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,25 +1,25 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        returnList = list()
-        nums_sorted = sorted(nums)
-        for i in range(len(nums_sorted)-2):
-            # check if cur is same as prev, we skip it as the triplets for the i has already been calculated
-            if i >0 and  nums_sorted[i] == nums_sorted[i-1]:
+        res = []
+        nums.sort()
+        for i in range(len(nums)-2):
+            # check for nums[i] duplicates only after 1st iteration to prevent negative index
+            if i>0 and nums[i] == nums[i-1]:
                 continue
-            l,r=i+1,len(nums_sorted)-1
+            l,r = i+1, len(nums)-1
             while l<r:
-                total = nums_sorted[i]+nums_sorted[l]+nums_sorted[r]
+                total = nums[i] + nums[l] + nums[r]
                 if total == 0:
-                    returnList.append((nums_sorted[i],nums_sorted[l],nums_sorted[r]))
-                    # skipping dupes for left and right pointer
-                    while l<r and nums_sorted[l] == nums_sorted[l+1]:
-                        l += 1
-                    while l<r and nums_sorted[r] == nums_sorted[r-1]:
-                        r -= 1    
-                    l+=1
+                    res.append([nums[i], nums[l], nums[r]])
+                    # skip over dupes for l and r
+                    while l<r and nums[l] == nums[l+1]:
+                        l+=1
+                    while l<r and nums[r] == nums[r-1]:
+                        r-=1
                     r-=1
+                    l+=1
                 if total < 0:
                     l+=1
-                elif total > 0:
+                elif total >0:
                     r-=1
-        return returnList
+        return res
--- README.md
+++ README.md
@@ -10,6 +10,7 @@ Created using [LeetHub](https://github.com/QasimWani/LeetHub)
 |  |
 | ------- |
 | [0001-two-sum](https://github.com/alexusljf/LeetCode/tree/master/0001-two-sum) |
+| [0015-3sum](https://github.com/alexusljf/LeetCode/tree/master/0015-3sum) |
 | [0049-group-anagrams](https://github.com/alexusljf/LeetCode/tree/master/0049-group-anagrams) |
 | [0217-contains-duplicate](https://github.com/alexusljf/LeetCode/tree/master/0217-contains-duplicate) |
 | [0283-move-zeroes](https://github.com/alexusljf/LeetCode/tree/master/0283-move-zeroes) |
@@ -50,6 +51,7 @@ Created using [LeetHub](https://github.com/QasimWani/LeetHub)
 ## Two Pointers
 |  |
 | ------- |
+| [0015-3sum](https://github.com/alexusljf/LeetCode/tree/master/0015-3sum) |
 | [0125-valid-palindrome](https://github.com/alexusljf/LeetCode/tree/master/0125-valid-palindrome) |
 | [0283-move-zeroes](https://github.com/alexusljf/LeetCode/tree/master/0283-move-zeroes) |
 | [0345-reverse-vowels-of-a-string](https://github.com/alexusljf/LeetCode/tree/master/0345-reverse-vowels-of-a-string) |
@@ -150,6 +152,7 @@ Created using [LeetHub](https://github.com/QasimWani/LeetHub)
 ## Sorting
 |  |
 | ------- |
+| [0015-3sum](https://github.com/alexusljf/LeetCode/tree/master/0015-3sum) |
 | [0049-group-anagrams](https://github.com/alexusljf/LeetCode/tree/master/0049-group-anagrams) |
 | [0217-contains-duplicate](https://github.com/alexusljf/LeetCode/tree/master/0217-contains-duplicate) |
 | [0242-valid-anagram](https://github.com/alexusljf/LeetCode/tree/master/0242-valid-anagram) |
