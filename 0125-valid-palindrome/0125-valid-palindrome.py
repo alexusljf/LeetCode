715b3010fb6e7736720465c6e8807644b653ee4d
Alexus Lim <97541746+alexusljf@users.noreply.github.com>
2025-03-29 11:16:39 +0800

    0125-VALID-PALINDROME Stats: Time: 12 ms (19.42%), Space: 23.2 MB (16.7%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,7 +1,8 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        # create copy with only alphanumerics by using isalnum in list comprehension
-        sClean = ''.join([char.lower() for char in s if char.isalnum()])
+        # clean the string by removing non-alphanumeric chars and lowercase all chars
+        sClean = ''.join(char.lower() for char in s if char.isalnum())
+        print(sClean)
         l,r=0,len(sClean)-1
         while l<r:
             if sClean[l] != sClean[r]:
