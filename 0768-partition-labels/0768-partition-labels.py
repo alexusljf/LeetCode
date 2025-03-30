751ac34f22b2035af869603988999dd65a228443
Alexus Lim <97541746+alexusljf@users.noreply.github.com>
2025-03-31 01:31:18 +0800

    0768-PARTITION-LABELS Stats: Time: 0 ms (100%), Space: 17.7 MB (57.51%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.py
+++ 0768-partition-labels/0768-partition-labels.py
@@ -1,14 +1,34 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        lastOccurences = {}
-        for i,char in enumerate(s):
-            lastOccurences[char] = i
-        partitions = []
-        start,end = 0,0
-        for i,char in enumerate(s):
-            end = max(end, lastOccurences[char])
-            if i == end:
-                partitions.append(i-start+1)
-                start = i+1
-        return partitions
-            
+        # lastOccurences = {}
+        # for i,char in enumerate(s):
+        #     lastOccurences[char] = i
+        # partitions = []
+        # start,end = 0,0
+        # for i,char in enumerate(s):
+        #     end = max(end, lastOccurences[char])
+        #     if i == end:
+        #         partitions.append(i-start+1)
+        #         start = i+1
+        # return partitions
+        
+        # merge intervals way of doing
+        occurences = {} # store the first and last occurences
+        for i, char in enumerate(s):
+            if char not in occurences:
+                occurences[char] = [i,i]
+            else:
+                occurences[char][1] = i
+        intervals = sorted(occurences.values())
+        # init the first interval
+        partitions = [intervals[0]]
+        for i in range(1,len(intervals)):
+            # if cur start is <= last interval's end, then merge
+            if intervals[i][0] <= partitions[-1][1]:
+                # update the end of the last interval to max of cur and last end
+                partitions[-1] = [partitions[-1][0], max(intervals[i][1], partitions[-1][1])]
+            else:
+                # else add the interval entirely
+                partitions.append(intervals[i])
+                
+        return [end - start + 1 for start,end in partitions]
