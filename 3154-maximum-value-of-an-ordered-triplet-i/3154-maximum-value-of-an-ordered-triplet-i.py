730a8ff8ff87029c9aa36aab1937e8234bcdc5fd
Alexus Lim <97541746+alexusljf@users.noreply.github.com>
2025-04-03 10:47:02 +0800

    3154-MAXIMUM-VALUE-OF-AN-ORDERED-TRIPLET-I Stats: Time: 4 ms (70.59%), Space: 17.7 MB (82.11%) - LeetHub

--- 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.py
+++ 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.py
@@ -1,10 +1,13 @@
 class Solution:
     def maximumTripletValue(self, nums: List[int]) -> int:
+        # previously used 3 nested loop to scan thru which works here cause len <= 100
+        # use greedy approach to remove 1st loop, since we can use the max val for the first loop
         res = 0
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                for k in range(j+1,len(nums)):
-                    total = (nums[i] - nums[j]) * nums[k]
-                    if total >0:
-                        res = max(res,total)
+        left = nums[0]
+        for j in range(1, len(nums)):
+            if nums[j] > left: # overwrite the left ptr if nums[j] > left
+                left = nums[j]
+                continue
+            for k in range(j+1, len(nums)):
+                res = max(res,(left-nums[j])*nums[k])
         return res
