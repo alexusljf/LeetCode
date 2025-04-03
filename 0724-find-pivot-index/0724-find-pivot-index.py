8529b620779b13143f454486bd469ea79f4dece8
Alexus Lim <97541746+alexusljf@users.noreply.github.com>
2025-04-03 22:42:43 +0800

    0724-FIND-PIVOT-INDEX Stats: Time: 11 ms (31.66%), Space: 19.1 MB (10.2%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -1,20 +1,16 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
-        n = len(nums)
-        pivotLeftArr = [0] * n
-        pivotLeftArr[0] = nums[0]
-        for i in range(1, n):
-            pivotLeftArr[i] = pivotLeftArr[i-1] + nums[i]
-        print(pivotLeftArr)
-
-        pivotRightArr = [0] * n
-        # indexing of pivotRightArr same as LeftArr
-        pivotRightArr[n-1] = nums[n-1]
+        n=len(nums)
+        # init prefixSumLeft to start with first value, Right to start with last value
+        prefixSumLeft=[nums[0]] * n
+        prefixSumRight=[nums[n-1]] * n
+        for i in range(1,n):
+            prefixSumLeft[i] = nums[i] + prefixSumLeft[i-1]
         for i in range(n-2,-1,-1):
-            pivotRightArr[i] = pivotRightArr[i+1] + nums[i]
-        print(pivotRightArr)
-
+            # start from 2nd last to 0th index
+            prefixSumRight[i] = nums[i] + prefixSumRight[i+1] # i+1 since we counting backwards
         for i in range(n):
-            if pivotLeftArr[i] == pivotRightArr[i]:
+            # find the pivot index where both arr have same sum
+            if prefixSumRight[i] == prefixSumLeft[i]:
                 return i
         return -1
